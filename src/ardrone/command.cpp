61d443f57e3590dcbdea1d39d7a3521078069a37
soramamekun34 <pand.louis@gmail.com>
2020-02-12 17:20:27 +0900

    高さなど上限をプロ室用にした

--- src/ardrone/command.cpp
+++ src/ardrone/command.cpp
@@ -65,7 +65,7 @@ int ARDrone::initCommand(void)
 
         // Set maximum velocity in Z-axis [mm/s]
         sockCommand.sendf("AT*CONFIG_IDS=%d,\"%s\",\"%s\",\"%s\"\r", ++seq, ARDRONE_SESSION_ID, ARDRONE_PROFILE_ID, ARDRONE_APPLOCATION_ID);
-        sockCommand.sendf("AT*CONFIG=%d,\"control:control_vz_max\",\"%d\"\r", ++seq, 700);
+        sockCommand.sendf("AT*CONFIG=%d,\"control:control_vz_max\",\"%d\"\r", ++seq, 70);	//èâä˙ílÇÕ700
         msleep(100);
 
         // Set maximum yaw [rad/s]
@@ -80,7 +80,7 @@ int ARDrone::initCommand(void)
 
         // Set maximum altitude [mm]
         sockCommand.sendf("AT*CONFIG_IDS=%d,\"%s\",\"%s\",\"%s\"\r", ++seq, ARDRONE_SESSION_ID, ARDRONE_PROFILE_ID, ARDRONE_APPLOCATION_ID);
-        sockCommand.sendf("AT*CONFIG=%d,\"control:altitude_max\",\"%d\"\r", ++seq, 3000);
+        sockCommand.sendf("AT*CONFIG=%d,\"control:altitude_max\",\"%d\"\r", ++seq, 2000);	//èâä˙ílÇÕ3000
         msleep(100);
 
         // Bitrate control mode
